d192251451a581b18148c1fd88a607446145dad1
Dawood Khan <dawood@herocore.io>
2026-02-06 15:07:31 +0530

    feat(chat): integrating tawk.to chat client script with the app.

---
+++ apps/app/public/scripts/tawk.js
@@ -0,0 +1,9 @@
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+  var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+  s1.async=true;
+  s1.src='https://embed.tawk.to/69857705d634cb1c37ee2580/1jgolhrs2';
+  s1.charset='UTF-8';
+  s1.setAttribute('crossorigin','*');
+  s0.parentNode.insertBefore(s1,s0);
+})();
